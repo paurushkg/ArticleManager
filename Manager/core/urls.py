c0e63a53e3380add1cc9aa55bfed63f0d95a89e0
Paurush Kumar Gupta <gpaurush39@gmail.com>
2020-09-28 18:49:10 +0530

    Adds function based api view

--- Manager/core/urls.py
+++ Manager/core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import article_list
+from core.views import article_list, article_detail
 
 urlpatterns = [
     path('article/', article_list),
+    path('detail/<int:pk>', article_detail),
 ]
--- Manager/core/views.py
+++ Manager/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import HttpResponse
+
 from core.models import Article
 from core.serializers import ArticleSerializer
 
@@ -21,3 +23,26 @@ def article_list(request):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data, status=200)
+    
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=204)
+
