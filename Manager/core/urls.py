98181d873e6361c1f4b45ff0580877b70724253f
Paurush Kumar Gupta <gpaurush39@gmail.com>
2020-09-29 08:23:19 +0530

    Removes a Bug

--- Manager/core/urls.py
+++ Manager/core/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import article_list, article_detail
+from core.views import(
+    article_list,
+    article_detail,
+    ArticleAPIView,
+    ArticleDetail
+)
 
 urlpatterns = [
     path('article/', article_list),
+    path('Carticle/', ArticleAPIView.as_view()),
     path('detail/<int:pk>', article_detail),
+    path('Cdetail/<int:pk>', ArticleDetail.as_view()),
 ]
--- Manager/core/views.py
+++ Manager/core/views.py
@@ -1,13 +1,57 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.views import APIView
 from django.http import HttpResponse
 
 from core.models import Article
 from core.serializers import ArticleSerializer
 
 
+#Class Based API Views
+class ArticleAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+class ArticleDetail(APIView):
+
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return HttpResponse(status=404)
+        
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=204)
+
+
+#Function Based Views
 @api_view(['GET', 'POST'])
 def article_list(request):
 
